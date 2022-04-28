a0e9131f7e4361d765693a34a324e0dce8f0c20e
Luke Kluesner <luke.p.kluesner@gmail.com>
2022-04-28 14:43:54 -0500

    added about page button

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,23 @@ const IndexPage = () => {
     <div className="container">
       <title>Team MRLC</title>
       
-        <h1>Team MRLC</h1>
+      <h1>Team MRLC</h1>
+      <div className='imagecontainer'>
+
+      </div>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna lorem, congue et elit eu, consequat pretium libero. Nam imperdiet fringilla tristique. Morbi ut euismod tortor, id sagittis turpis. Proin euismod leo quis congue lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque ac mollis mi. Nam odio elit, efficitur et accumsan vitae, ultrices malesuada nisl. Vestibulum elementum mauris in tortor semper, molestie auctor augue laoreet.
       </p>
+      <div className="buttoncontainer">
+      <Link to="/about">
+      <button className="learn-more">
+        <span className="circle" aria-hidden="true">
+        <span className="icon arrow"></span>
+        </span>
+        <span className="button-text">Learn More</span>
+      </button>
+      </Link>
+      </div>
       
     </div>
     <div className="container">
@@ -21,6 +34,12 @@ const IndexPage = () => {
       <Link to="/ralphPortfolio"><button>R</button></Link>
       <Link to="/lukePortfolio"><button className="lukeButton">L</button></Link>
       <Link to="/chrisPortfolio"><button>C</button></Link>
+    </div>
+    <div className="container">
+      <h3>Matt</h3>
+      <h3>Ralph</h3>
+      <h3>Luke</h3>
+      <h3>Chris</h3>
     </div>
     
     </main>
