30e49b2cce627deceb06ef1abb8d1eb48a985c9e
matthegreat8 <matthegreat8@gmail.com>
2022-04-28 17:10:30 -0500

    Update mattPortfolio.js

--- src/pages/mattPortfolio.js
+++ src/pages/mattPortfolio.js
@@ -28,6 +28,8 @@ const MattPage = () => {
           <title>Matt</title>
           
             <h1>Matt</h1>
+            <h2>About</h2>
+        
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna lorem, congue et elit eu, consequat pretium libero. Nam imperdiet fringilla tristique. Morbi ut euismod tortor, id sagittis turpis. Proin euismod leo quis congue lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque ac mollis mi. Nam odio elit, efficitur et accumsan vitae, ultrices malesuada nisl. Vestibulum elementum mauris in tortor semper, molestie auctor augue laoreet.
           </p>
