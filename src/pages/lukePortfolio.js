2233482e8a447f5d086601f4946ee246c4580023
Luke Kluesner <luke.p.kluesner@gmail.com>
2022-04-28 12:46:47 -0500

    comments working (only on hosted website)

--- src/pages/lukePortfolio.js
+++ src/pages/lukePortfolio.js
@@ -15,20 +15,13 @@ const LukePage = () => {
     return (
         <main>
         <div className="container">
-          <title>Team MRLC</title>
+          <title>Luke</title>
           
-            <h1>Team MRLC</h1>
+            <h1>Luke</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna lorem, congue et elit eu, consequat pretium libero. Nam imperdiet fringilla tristique. Morbi ut euismod tortor, id sagittis turpis. Proin euismod leo quis congue lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque ac mollis mi. Nam odio elit, efficitur et accumsan vitae, ultrices malesuada nisl. Vestibulum elementum mauris in tortor semper, molestie auctor augue laoreet.
           </p>
           
-        </div>
-        <div className="container">
-          <h1>Our Team</h1>
-          <Link to="/mattPortfolio"><button>M</button></Link>
-          <Link to="/ralphPortfolio"><button>R</button></Link>
-          <Link to="/lukePortfolio"><button className="lukeButton">L</button></Link>
-          <Link to="/chrisPortfolio"><button>C</button></Link>
         </div>
         <DiscussionEmbed shortname={"team-mrlc-site"} config={disqusConfig} />
         </main>
--- src/pages/mattPortfolio.js
+++ src/pages/mattPortfolio.js
@@ -1,23 +1,29 @@
 import * as React from 'react'
-import Layout from '../components/layout'
 import "./../styles/global.css"
 import { Link } from 'gatsby'
 
+const disqusConfig = {
+    identifier: 'mattPortfolio', // you can define anything as "identifier" for each blog post
+    title: 'Matt Portfolio',
+    url: 'https://teammrlcsitemain.gatsbyjs.io/mattPortfolio', 
+  }
+
 
 const MattPage = () => {
-  return (
-    <Layout pageTitle="MATT">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna lorem, congue et elit eu, consequat pretium libero. Nam imperdiet fringilla tristique. Morbi ut euismod tortor, id sagittis turpis. Proin euismod leo quis congue lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque ac mollis mi. Nam odio elit, efficitur et accumsan vitae, ultrices malesuada nisl. Vestibulum elementum mauris in tortor semper, molestie auctor augue laoreet.
-      </p>
-    <Layout pageTitle="Our Team">
-      <Link to="../mattPortfolio"><button>Matt</button></Link>
-      <Link to="../ralphPortfolio"><button>Ralph</button></Link>
-      <Link to="../lukePortfolio"><button>Luke</button></Link>
-      <Link to="../chrisPortfolio"><button>Chris</button></Link>
-    </Layout>
-    </Layout>
-
-  )
+    return (
+        <main>
+        <div className="container">
+          <title>Matt</title>
+          
+            <h1>Matt</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna lorem, congue et elit eu, consequat pretium libero. Nam imperdiet fringilla tristique. Morbi ut euismod tortor, id sagittis turpis. Proin euismod leo quis congue lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque ac mollis mi. Nam odio elit, efficitur et accumsan vitae, ultrices malesuada nisl. Vestibulum elementum mauris in tortor semper, molestie auctor augue laoreet.
+          </p>
+          
+        </div>
+        <DiscussionEmbed shortname={"team-mrlc-site"} config={disqusConfig} />
+        </main>
+    
+      )
 }
 export default MattPage
