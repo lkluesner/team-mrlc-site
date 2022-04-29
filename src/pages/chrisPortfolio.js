e5bde81ea511f5491a5974b7217745534b134948
Chris Mott <chrismott@uiowa.edu>
2022-04-28 19:40:52 -0500

    adding to my projects portion

--- src/pages/chrisPortfolio.js
+++ src/pages/chrisPortfolio.js
@@ -33,7 +33,7 @@ const ChrisPage = () => {
 
         </div>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna lorem, congue et elit eu, consequat pretium libero. Nam imperdiet fringilla tristique. Morbi ut euismod tortor, id sagittis turpis. Proin euismod leo quis congue lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque ac mollis mi. Nam odio elit, efficitur et accumsan vitae, ultrices malesuada nisl. Vestibulum elementum mauris in tortor semper, molestie auctor augue laoreet.
+        I am a Senior, double major in Computer Science and Computer Engineering. My goal is to be a member of a cutting-edge company that will allow me to build on my experience to develop, and maintain a wide variety of software applications, exposure to artificial intelligence, machine learning, coding and critical thinking. Ideally, I would like an Internship or Co-Op, in or out of state, that can offer an ongoing relationship that will expand and grow as my knowledge, skill set and coursework becomes more specialized with an opportunity for employment after graduation. 
         </p>
         <div className='imagecontainer lukePic2'>
 
@@ -42,8 +42,15 @@ const ChrisPage = () => {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna lorem, congue et elit eu, consequat pretium libero. Nam imperdiet fringilla tristique. Morbi ut euismod tortor, id sagittis turpis. Proin euismod leo quis congue lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque ac mollis mi. Nam odio elit, efficitur et accumsan vitae, ultrices malesuada nisl. Vestibulum elementum mauris in tortor semper, molestie auctor augue laoreet.
         </p>
         <h2>Projects</h2>
+        <h3>IoT Mechanical Chess Board</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum magna lorem, congue et elit eu, consequat pretium libero. Nam imperdiet fringilla tristique. Morbi ut euismod tortor, id sagittis turpis. Proin euismod leo quis congue lacinia. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque ac mollis mi. Nam odio elit, efficitur et accumsan vitae, ultrices malesuada nisl. Vestibulum elementum mauris in tortor semper, molestie auctor augue laoreet.
+          Thanks to the netflix series "Queens Gambit", myself and a few buddies have enjoyed
+          learning the game chess. We learned by playing online chess, which is fun and all, but lacks some
+          important aspects that a physical game provides. This motivated us to create a mechanical chess
+          board capable of sliding chess pieces accross the board to play the human user. This project has 
+          allowed us to use different aspects of Computer Science we've learned throughout the year like 
+          electric stepper motors, path finding algorithms and AI enabled chess engines. We were successful in 
+          presenting a function prototype and enjoyed engineering the product from scratch.
         </p>
         
       </div>
