2b2874afb9077b154a5f30d5d9f787cc4defcb9c
Luke Kluesner <luke.p.kluesner@gmail.com>
2022-04-28 21:27:57 -0500

    about outline

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,38 +18,22 @@ const AboutPage = () => {
         <div className="container">
           <title>Luke</title>
           
-          <h1>Luke</h1>
-          <h2>About</h2>
+          <h1>About Our Team</h1>
+          <h2>About Our Team</h2>
+          <div className='imagecontainer'>
+          </div>
+          <div className="container">
+          <h1>Our Members</h1>
+          <Link to="/mattPortfolio"><button className="mattButton imageButton">M</button></Link>
+          <Link to="/ralphPortfolio"><button className="ralphButton imageButton">R</button></Link>
+          <Link to="/chrisPortfolio"><button className="chrisButton imageButton">C</button></Link>
+          <Link to="/lukePortfolio"><button className="lukeButton imageButton">L</button></Link>
+          </div>
+          <p>
+
+          </p>
           
-          <p>
-            My name is Luke Kluesner and I am a senior at the University of Iowa. My major is Computer Science and Engineering with a focus in Software Engineering. I love to hike and ski whenever I get the chance! In my free time you can usually find me hanging out with my friends. I love being outside and building things!
-          </p>
-          <div className='imagecontainer lukePic1'>
-          </div>
-          <br></br>
-          <div className='imagecontainer lukePic2'>
-          </div>
-          <p>
-            The pictures above are of me hiking Camelback Mountain in Arizona, and of me on a ski trip in Aspen, CO.
-          </p>
-          <h2>Projects</h2>
-          <div className='imagecontainer lukePic3'>
-          </div>
-          <br></br>
-          <h3>IOT Automated Chessboard</h3>
-          <p>
-            One of the projects I'm working on is an IOT Automated Chessboard. This project combines the feel of real chess board, with the convience of online chess. Users will be able to play on a real chess board with someone hundreds of miles away. They will also be able to replay famous chess matches from history and see them played in front of their eyes. Lastly they can play an AI opponent on a real chess board with ease. This project works by having an XY plotter, I designed, control the movement of the pieces using magnets embedded in the pieces. Majority of the pieces are 3d printed. The XY table is controlled by custom arduino code using the AccelStepper library, and the chess game is controlled on a raspberry pi utilizing the stockfish engine. This project picture is of it still in progress, and the final prototype will be much nicer.
-          </p>
-          <div className='imagecontainer lukePic4'>
-          </div>
-          <br></br>
-          <h3>Uiowa Hospitals HCIS "ipmac" Project</h3>
-          <p>
-            Another Project I am working on is for the University of Iowa Hospitals and Clinics Network Team. This project scans the hospital network using an SNMP script I wrote using PySNMP to get the devices connected to the network. This then stores this data in a timestamped MySQL server. This project also includes a multitude of different scripts to search the data stored in the MySQL server to help the Network Team diagnose problems faster. It has been a fun project where I gained alot of experience developing software in a professional enviroment.
-          </p>
-          <br></br>
-          <p>You can reach me on <Link to="https://www.linkedin.com/in/luke-kluesner-1316211b9/">LinkedIn.</Link></p>
-          <br></br>
+          
         </div>
         </main>
   )
