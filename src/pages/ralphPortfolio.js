9aea690215cae7eb0bbbc16cf8549e3440b01049
Ralph Nahra <ralph-nahra@uiowa.edu>
2022-04-28 20:19:40 -0500

    add technologies showcase section

--- src/pages/ralphPortfolio.js
+++ src/pages/ralphPortfolio.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import "./../styles/global.css"
+import "./../styles/ralph.css"
 import { Link } from 'gatsby'
 import { Disqus } from 'gatsby-plugin-disqus'
 import { DiscussionEmbed } from 'disqus-react'
@@ -13,6 +14,10 @@ const disqusConfig = {
 const RalphPage = () => {
     return (
       <main>
+        
+        {/* Font Awesome css */}
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous"></link>
+        
         <div className="backbuttoncontainer">
             <Link to="../">
             <button className="back-button">
@@ -36,7 +41,48 @@ const RalphPage = () => {
             systems programming, and open-source software. I have a breadth of experiences in software development, from working on modern
             web applications to embedded systems. In my free time I enjoy playing video games, skateboarding, and playing guitar.
           </p>
-          
+
+          <p>I have experience with...</p>
+
+          <section id="technologies">
+            
+            <div class="techrow">
+
+              <div class="html5" id="xd">
+                  <i class="fab fa-html5"></i>
+                  <strong>HTML5</strong>
+              </div> 
+              
+              <div class="css"> 
+                  <i class="fab fa-css3-alt"></i>
+                  <strong>CSS3</strong>
+              </div>
+
+              <div class="js">
+                  <i class="fab fa-js-square"></i>
+                  <strong>JavaScript</strong> 
+              </div>
+
+              <div class="and">
+                  <i class="fab fa-android"></i>
+                  <strong>Android</strong>
+              </div>
+
+              <div class="java">
+                  <i class="fab fa-java"></i>
+                  <strong>Java</strong>
+              </div>
+
+
+              <div class="python">
+                  <i class="fab fa-python"></i>
+                  <strong>Python</strong>
+              </div>
+              
+            </div>
+
+          </section>
+
         </div>
         <div className='disquscontainer'>
         <DiscussionEmbed shortname={"team-mrlc-site"} config={disqusConfig} />
